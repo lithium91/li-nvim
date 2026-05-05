b295075b86e02f7c2b12e9a78d2aeccc10c80c7c
lithium91 <aleinikow.nikita@yandex.ru>
2026-05-05 11:23:19 +0300

    move lines on A+J/K

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -9,3 +9,6 @@ end, { desc = "Open Oil" })
 
 vim.keymap.set("n", "q", "<nop>", { noremap = true, silent = true })
 vim.keymap.set("n", "@", "<nop>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==")
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==")
